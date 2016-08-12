50c5753d2b8ed02f566dcf2707505468b1c16477
Dante2001 <andrianodia@hotmail.com>
2016-08-11 22:36:20 -0300

    added suicider/zigzag/circle

--- Assets/EnemySuicider.cs
+++ Assets/EnemySuicider.cs
@@ -10,20 +10,29 @@ public class EnemySuicider : MonoBehaviour {
 	public int enemyType = -1;
 	public int speedMultiplier=1;
 	private Vector3 speed = Vector3.zero;
-
+	private Vector3 center = Vector3.zero;
+	private float currentAngle = 0;
+	private float radius = 1f;
 	// Use this for initialization
 	void Start () {
 		if (enemyType == -1) {
 			enemyType = Random.Range (0,2);
 		}
-		enemyType = ENEMY_SUICIDER;
-		if (enemyType == ENEMY_SUICIDER) {
+		enemyType = ENEMY_ZIGZAG;
+		switch (enemyType) {
+		case ENEMY_SUICIDER:
 			Vector3 target = GameObject.FindGameObjectWithTag("Player").transform.position;
 			speed = new Vector3((target.x - transform.position.x)/100*speedMultiplier,(target.y - transform.position.y)/100*speedMultiplier,0);
 			Vector3 vectorToTarget = target - transform.position;
 			float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
 			Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
 			transform.rotation = q;
+			break;
+		case ENEMY_ZIGZAG:
+		case ENEMY_CIRCLE:
+			center = transform.position;
+			currentAngle = 0;
+			break;
 		}
 	}
 	
@@ -33,7 +42,16 @@ public class EnemySuicider : MonoBehaviour {
 			case ENEMY_SUICIDER:
 			transform.position = transform.position + speed;
 			break;
-
+		case ENEMY_ZIGZAG:
+			transform.position = new Vector3 (center.x+Mathf.Cos(currentAngle) * radius,center.y,0 );
+			center = new Vector3(center.x,center.y-.05f,center.z);
+			currentAngle += .1f;
+			break;
+		case ENEMY_CIRCLE:
+			transform.position = new Vector3 (center.x+Mathf.Cos(currentAngle) * radius,center.y+Mathf.Sin(currentAngle) * radius,0 );
+			center = new Vector3(center.x,center.y-.05f,center.z);
+			currentAngle += .1f;
+			break;
 		}
 	}
 }
