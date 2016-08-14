7b4e0178fd1b19ae23c750877fa62c3c03f0228c
Dante2001 <andrianodia@hotmail.com>
2016-08-14 17:48:39 -0300

    added small effects

---
+++ Assets/SmartMachinegun.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmartMachinegun : MonoBehaviour {
+
+	public GameObject bullet;
+	public float rechargeTime=0f;
+	public float firingTime=0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
