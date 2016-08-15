e5e4537582f0700b8fcda14b7cd005fe16d3f9c9
Dante2001 <andrianodia@hotmail.com>
2016-08-15 16:43:41 -0300

    destroy self stuff and pew pew effect

---
+++ Assets/DestroySelf.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroySelf : MonoBehaviour {
+
+	public float timeToBeDestroyed = 5f;
+	void selfDestroy() {
+		Destroy (this.gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		Invoke ("selfDestroy", timeToBeDestroyed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
