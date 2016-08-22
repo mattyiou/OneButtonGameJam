33852d05f48a260856599de17afaaa82552d93cb
Dante2001 <andrianodia@hotmail.com>
2016-08-22 00:14:27 -0300

    added main menu

--- Assets/CameraShake.cs
+++ Assets/CameraShake.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class CameraShake : MonoBehaviour {
 
+	public void shake() {
+
+
+	}
+
 	// Use this for initialization
 	void Start () {
 	
-	}
-	
+
+	} 
+
+
 	// Update is called once per frame
 	void Update () {
 	
